dc5a988cdb66e93b9f85c0aa7423f8cd981dfa2a
Jakub Wiśniowski <jakub.e.wisniowski@gmail.com>
2017-09-08 11:49:36 +0200

    first commit

---
+++ src/main/java/hello/Customer.java
@@ -0,0 +1,52 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class// tworzy tabelke z tej nazwy!!
+public class Customer {
+    @Id // !!primaryKey
+    @GeneratedValue(strategy=GenerationType.AUTO) // !!autoicrements
+    private Long id;
+    private String name;
+    private String address;
+    private String countryCode;
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+    
+    
+}
+
